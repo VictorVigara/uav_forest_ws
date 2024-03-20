ac1cae878f1f79d0bf9babe12fa722953acc5210
VictorVigara <vigaravictor99@gmail.com>
2024-03-20 14:13:03 +0100

    px4_msgs working and rosbag command to record

---
+++ rosbag.sh
@@ -0,0 +1,3 @@
+ros2 bag record /camera/camera/color/image_raw /camera/camera/depth/image_rect_raw /teraranger_evo_40m /livox/lidar /livox/imu /fmu/out/sensor_combined /fmu/out/vehicle_global_position /fmu/out/vehicle_gps_position /fmu/out/vehicle_local_position /fmu/out/vehicle_odometry
+
+
