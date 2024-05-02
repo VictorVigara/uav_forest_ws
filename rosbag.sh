0adf8e74a981585df4bb3da889191663e6adc3d6
VictorVigara <vigaravictor99@gmail.com>
2024-05-02 10:20:23 +0200

    gitignore updated to not push rosbags

--- rosbag.sh
+++ rosbag.sh
@@ -1,3 +1,4 @@
-ros2 bag record /camera/camera/color/image_raw /camera/camera/depth/image_rect_raw /teraranger_evo_40m /livox/lidar /livox/imu /fmu/out/sensor_combined /fmu/out/vehicle_global_position /fmu/out/vehicle_gps_position /fmu/out/vehicle_local_position /fmu/out/vehicle_odometry
+sleep 5
+ros2 bag record /camera/camera/color/image_raw /camera/camera/depth/image_rect_raw /teraranger_evo_40m /livox/lidar /livox/imu /fmu/out/sensor_combined /fmu/out/vehicle_global_position /fmu/out/vehicle_gps_position /fmu/out/vehicle_local_position /fmu/out/vehicle_odometry /fmu/out/vehicle_attitude
 
 
